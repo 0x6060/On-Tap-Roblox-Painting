63d7347ea1327663059f5125437acb6db6cbf0f2
Teddie <177896454+0x6060@users.noreply.github.com>
2025-08-14 12:02:54 +0200

    Update basex.lua

--- basex.lua
+++ basex.lua
@@ -5,19 +5,22 @@
 local basex, alphabets, basex_meta, basex_instance_meta
 
 alphabets = {
-  BASE16LOWER = '0123456789abcdef',
-  BASE16UPPER = '0123456789ABCDEF',
-  BASE58BITCOIN = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
-  BASE58FLICKR = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ',
-  BASE58RIPPLE = 'rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz',
+	BASE16LOWER = '0123456789abcdef',
+	BASE16UPPER = '0123456789ABCDEF',
+	BASE58BITCOIN = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+	BASE58FLICKR = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ',
+	BASE58RIPPLE = 'rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz',
+	DISCORD = [[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!$%&()+,./:;<=?@^{|}"]],
+	DISCORDZ = [[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!$%&()+,./:;<=?@^{|}��������"����������������������������������������������������������������������������������������������������������������]],                        
+	BASE91 = [[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~"]], 
 }
 
 basex = {
-  _VERSION = '0.2.0',
-  _URL = 'https://github.com/un-def/lua-basex',
-  _DESCRIPTION = 'Base encoding/decoding of any given alphabet ' ..
-                 'using bitcoin style leading zero compression',
-  _LICENSE = [[
+	_VERSION = '0.2.0',
+	_URL = 'https://github.com/un-def/lua-basex',
+	_DESCRIPTION = 'Base encoding/decoding of any given alphabet ' ..
+		'using bitcoin style leading zero compression',
+	_LICENSE = [[
     Copyright (c) 2016-2017, un.def
     All rights reserved.
 
@@ -42,113 +45,113 @@ basex = {
     OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
     DAMAGE.
   ]],
-  alphabets = alphabets,
+	alphabets = alphabets,
 }
 
 
 basex_meta = {
-  __call = function(_, alphabet)
-    local alphabet_map = {}
-    local base = #alphabet
-    local leader = alphabet:sub(1, 1)
-    for i = 1, base do
-      alphabet_map[alphabet:sub(i, i)] = i - 1
-    end
-    local basex_instance = {
-      alphabet = alphabet,
-      base = base,
-      leader = leader,
-      alphabet_map = alphabet_map,
-    }
-    return setmetatable(basex_instance, basex_instance_meta)
-  end,
+	__call = function(_, alphabet)
+		local alphabet_map = {}
+		local base = #alphabet
+		local leader = alphabet:sub(1, 1)
+		for i = 1, base do
+			alphabet_map[alphabet:sub(i, i)] = i - 1
+		end
+		local basex_instance = {
+			alphabet = alphabet,
+			base = base,
+			leader = leader,
+			alphabet_map = alphabet_map,
+		}
+		return setmetatable(basex_instance, basex_instance_meta)
+	end,
 
-  __index = function(cls, key)
-    local alphabet = cls.alphabets[key:upper()]
-    if not alphabet then return nil end
-    local basex_instance = cls(alphabet)
-    cls[key] = basex_instance
-    return basex_instance
-  end
+	__index = function(cls, key)
+		local alphabet = cls.alphabets[key:upper()]
+		if not alphabet then return nil end
+		local basex_instance = cls(alphabet)
+		cls[key] = basex_instance
+		return basex_instance
+	end
 }
 
 
 basex_instance_meta = {
-  __index = {
-    encode = function(self, source)
-      if #source == 0 then return '' end
+	__index = {
+		encode = function(self, source)
+			if #source == 0 then return '' end
 
-      local digits = {0}
+			local digits = {0}
 
-      for i = 1, #source do
-        local carry = source:byte(i, i)
-        for j = 1, #digits do
-          carry = carry + digits[j] * 256
-          digits[j] = carry % self.base
-          carry = math.floor(carry / self.base)
-        end
-        while carry > 0 do
-           table.insert(digits, carry % self.base)
-           carry = math.floor(carry / self.base)
-        end
-      end
+			for i = 1, #source do
+				local carry = source:byte(i, i)
+				for j = 1, #digits do
+					carry = carry + digits[j] * 256
+					digits[j] = carry % self.base
+					carry = math.floor(carry / self.base)
+				end
+				while carry > 0 do
+					table.insert(digits, carry % self.base)
+					carry = math.floor(carry / self.base)
+				end
+			end
 
-      for k = 1, #source-1 do
-        if source:byte(k, k) ~= 0 then break end
-        table.insert(digits, 0)
-      end
+			for k = 1, #source-1 do
+				if source:byte(k, k) ~= 0 then break end
+				table.insert(digits, 0)
+			end
 
-      local ii = 1
-      local jj = #digits
-      local tmp
-      while true do
-        tmp = self.alphabet:sub(digits[ii]+1, digits[ii]+1)
-        digits[ii] = self.alphabet:sub(digits[jj]+1, digits[jj]+1)
-        digits[jj] = tmp
-        ii = ii + 1
-        jj = jj - 1
-        if ii > jj then break end
-      end
+			local ii = 1
+			local jj = #digits
+			local tmp
+			while true do
+				tmp = self.alphabet:sub(digits[ii]+1, digits[ii]+1)
+				digits[ii] = self.alphabet:sub(digits[jj]+1, digits[jj]+1)
+				digits[jj] = tmp
+				ii = ii + 1
+				jj = jj - 1
+				if ii > jj then break end
+			end
 
-      return table.concat(digits)
-    end,
+			return table.concat(digits)
+		end,
 
-    decode = function(self, str)
-      if #str == 0 then return '' end
+		decode = function(self, str)
+			if #str == 0 then return '' end
 
-      local bytes = {0}
+			local bytes = {0}
 
-      local value, carry
-      for i = 1, #str do
-        value = self.alphabet_map[str:sub(i, i)]
-        if value == nil then
-            return nil, 'Non-base' .. self.base .. ' character'
-        end
-        carry = value
-        for j = 1, #bytes do
-          carry = carry + bytes[j] * self.base
-          bytes[j] = carry % 256
-          carry = math.floor(carry / 256)
-        end
-        while carry > 0 do
-          table.insert(bytes, carry % 256)
-          carry = math.floor(carry / 256)
-        end
-      end
+			local value, carry
+			for i = 1, #str do
+				value = self.alphabet_map[str:sub(i, i)]
+				if value == nil then
+					return nil, 'Non-base' .. self.base .. ' character'
+				end
+				carry = value
+				for j = 1, #bytes do
+					carry = carry + bytes[j] * self.base
+					bytes[j] = carry % 256
+					carry = math.floor(carry / 256)
+				end
+				while carry > 0 do
+					table.insert(bytes, carry % 256)
+					carry = math.floor(carry / 256)
+				end
+			end
 
-      for k = 1, #str-1 do
-        if str:sub(k, k) ~= self.leader then break end
-        table.insert(bytes, 0)
-      end
+			for k = 1, #str-1 do
+				if str:sub(k, k) ~= self.leader then break end
+				table.insert(bytes, 0)
+			end
 
-      local decoded = ''
-      for i = #bytes, 1, -1 do
-        decoded = decoded .. string.char(bytes[i])
-      end
+			local decoded = ''
+			for i = #bytes, 1, -1 do
+				decoded = decoded .. string.char(bytes[i])
+			end
 
-      return decoded
-    end
-  }
+			return decoded
+		end
+	}
 }
 
 
